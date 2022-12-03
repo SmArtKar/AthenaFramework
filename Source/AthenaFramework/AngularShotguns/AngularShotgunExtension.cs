94905e598d4bd767db7b080e5cf3f55f4b4bb17a
SmArtKar <smart.karpov@gmail.com>
2022-12-03 14:49:52 +0300

    First commit

---
+++ Source/AthenaFramework/AngularShotguns/AngularShotgunExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace AthenaFramework
+{
+    public class AngularShotgunExtension : DefModExtension
+    {
+        public int pelletCount;
+        public float pelletAngle;
+        public float downedHitChance = 0.20f;
+    }
+}
---
+++ Source/AthenaFramework/AngularShotguns/Verb_ShootAngularShotgun.cs
@@ -0,0 +1,214 @@
+﻿using Mono.Unix.Native;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace AthenaFramework
+{
+    public class Verb_ShootAngularShotgun : Verb_LaunchProjectile
+    {
+        private List<IntVec3> cachedTargetCells = new List<IntVec3>();
+        private IntVec3 cachedTargetPosition;
+        private IntVec3 cachedCasterPosition;
+
+        protected override int ShotsPerBurst => verbProps.burstShotCount;
+
+        public override void WarmupComplete()
+        {
+            base.WarmupComplete();
+            Pawn pawn = currentTarget.Thing as Pawn;
+            if (pawn != null && !pawn.Downed && !pawn.IsColonyMech && CasterIsPawn && CasterPawn.skills != null)
+            {
+                float friendlyModifier = (pawn.HostileTo(caster) ? 170f : 20f);
+                float cycleTimeModifier = verbProps.AdjustedFullCycleTime(this, CasterPawn);
+                CasterPawn.skills.Learn(SkillDefOf.Shooting, friendlyModifier * cycleTimeModifier, false);
+            }
+        }
+
+        public override void DrawHighlight(LocalTargetInfo target)
+        {
+            base.DrawHighlight(target);
+
+            if (target == null || target.Cell == null)
+            {
+                return;
+            }
+
+            if (cachedCasterPosition == caster.Position && cachedTargetPosition == target.Cell)
+            {
+                GenDraw.DrawFieldEdges(cachedTargetCells);
+                return;
+            }
+
+            cachedTargetCells = new List<IntVec3>();
+            cachedCasterPosition = caster.Position;
+            cachedTargetPosition = target.Cell;
+
+            AngularShotgunExtension extension = EquipmentSource.def.GetModExtension<AngularShotgunExtension>();
+            float angle = (float)Math.Acos(Vector2.Dot((new Vector2(currentTarget.Cell.x, currentTarget.Cell.z) - new Vector2(caster.Position.x, caster.Position.z)).normalized, new Vector2(1, 0)));
+            float pelletAngle = (float)(extension.pelletAngle * (Math.PI) / 180);
+
+            for (int i = 0; i < extension.pelletCount - 1; i++)
+            {
+
+                float newAngle = angle - pelletAngle * (extension.pelletCount - 1) / 2 + i * pelletAngle;
+                IntVec3 rangeEndPosition = (new IntVec3((int)(Math.Cos(newAngle) * verbProps.range), 0, (int)(Math.Sin(newAngle) * verbProps.range))) + caster.Position;
+
+                List<IntVec3> targetedCells = getHitTiles(caster.Position, rangeEndPosition, caster.Map);
+                cachedTargetCells = cachedTargetCells.Concat(targetedCells).ToList();
+            }
+
+            GenDraw.DrawFieldEdges(cachedTargetCells);
+        }
+
+        public List<IntVec3> getHitTiles(IntVec3 startPosition, IntVec3 endPosition, Map map)
+        {
+            List<IntVec3> cellList = new List<IntVec3>();
+
+            foreach (IntVec3 targetPosition in GenSight.PointsOnLineOfSight(startPosition, endPosition))
+            {
+                if (targetPosition == startPosition)
+                {
+                    cellList.Add(targetPosition);
+                    continue;
+                }
+
+                Thing targetBuilding = targetPosition.GetRoofHolderOrImpassable(map);
+                if (targetBuilding != null)
+                {
+                    break;
+                }
+
+                cellList.Add(targetPosition);
+                bool foundPawn = false;
+
+                foreach (Pawn pawnTarget in GridsUtility.GetThingList(targetPosition, map))
+                {
+                    if (!pawnTarget.Downed && !pawnTarget.Dead)
+                    {
+                        foundPawn = true;
+                        break;
+                    }
+                }
+
+                if (foundPawn)
+                {
+                    break;
+                }
+            }
+
+            return cellList;
+        }
+
+        protected override bool TryCastShot()
+        {
+            if (EquipmentSource == null || currentTarget.Cell == null)
+            {
+                return false;
+            }
+
+            AngularShotgunExtension extension = EquipmentSource.def.GetModExtension<AngularShotgunExtension>();
+
+            if (extension == null)
+            {
+                return false;
+            }
+
+            bool flag = base.TryCastShot();
+            if (!flag)
+            {
+                return false;
+            }
+
+            float angle = (float)Math.Acos(Vector2.Dot((new Vector2(currentTarget.Cell.x, currentTarget.Cell.z) - new Vector2(caster.Position.x, caster.Position.z)).normalized, new Vector2(1, 0)));
+            float pelletAngle = (float)(extension.pelletAngle * (Math.PI) / 180);
+
+            for (int i = 0; i < extension.pelletCount - 1; i++)
+            {
+                if (i == (extension.pelletCount - 1) / 2 + 1) //Skipping the central pellet that was already shot
+                {
+                    continue;
+                }
+
+                float newAngle = angle - pelletAngle * (extension.pelletCount - 1) / 2 + i * pelletAngle;
+                IntVec3 rangeEndPosition = (new IntVec3((int)(Math.Cos(newAngle) * verbProps.range), 0, (int)(Math.Sin(newAngle) * verbProps.range))) + caster.Position;
+                Thing newTarget = null;
+
+                foreach (IntVec3 targetPosition in GenSight.PointsOnLineOfSight(caster.Position, rangeEndPosition))
+                {
+                    if (targetPosition == caster.Position) //Prevents the caster from shooting himself
+                    {
+                        continue;
+                    }
+
+                    Thing targetBuilding = targetPosition.GetRoofHolderOrImpassable(caster.Map);
+                    if (targetBuilding != null)
+                    {
+                        newTarget = targetBuilding;
+                        break;
+                    }
+
+                    Thing cover = targetPosition.GetCover(caster.Map);
+                    if (cover != null)
+                    {
+                        if (Rand.Chance(cover.BaseBlockChance()))
+                        {
+                            newTarget = targetBuilding;
+                            break;
+                        }
+                    }
+
+                    bool foundPawn = false;
+                    foreach (Pawn pawnTarget in GridsUtility.GetThingList(targetPosition, caster.Map))
+                    {
+                        if (pawnTarget.Downed || pawnTarget.Dead)
+                        {
+                            if (Rand.Chance(extension.downedHitChance))
+                            {
+                                newTarget = pawnTarget;
+                                foundPawn = true;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            newTarget = pawnTarget;
+                            foundPawn = true;
+                            break;
+                        }
+                    }
+
+                    if (foundPawn)
+                    {
+                        break;
+                    }
+                }
+
+                if (newTarget != null)
+                {
+                    LocalTargetInfo newTargetInfo = new LocalTargetInfo(newTarget);
+                    currentTarget = newTargetInfo;
+                    currentDestination = newTargetInfo;
+                    base.TryCastShot();
+                }
+                else
+                {
+                    if (!base.TryFindShootLineFromTo(caster.Position, new LocalTargetInfo(rangeEndPosition), out ShootLine shootLine))
+                    {
+                        continue;
+                    }
+
+                    Projectile projectile = GenSpawn.Spawn(Projectile, shootLine.Source, caster.Map, WipeMode.Vanish) as Projectile;
+                    projectile.Launch(caster, caster.DrawPos, rangeEndPosition, currentTarget, ProjectileHitFlags.All, preventFriendlyFire, EquipmentSource, null);
+                }
+            }
+
+            return true;
+        }
+    }
+}
